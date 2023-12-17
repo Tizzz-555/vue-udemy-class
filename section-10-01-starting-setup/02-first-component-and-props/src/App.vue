fdfca67234f9065eb624e06dd2b8d40366c224ef
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-12-17 19:03:13 +0100

    project cards and header

--- section-10-01-starting-setup/02-first-component-and-props/src/App.vue
+++ section-10-01-starting-setup/02-first-component-and-props/src/App.vue
@@ -3,7 +3,9 @@
     <learning-resource
       v-for="res in storedResources"
       :key="res.id"
-      :resTitle="res.title"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
     ></learning-resource>
   </ul>
 </template>
@@ -21,14 +23,14 @@ export default {
         {
           id: 'official-guide',
           title: 'Official Guide',
-          description: 'The official Vue.js documentation',
+          description: 'The official Vue.js documentation.',
           link: 'https://vuejs.org',
         },
         {
           id: 'google',
           title: 'Google',
           description: 'Learn to google...',
-          link: 'https://google.com',
+          link: 'https://google.org',
         },
       ],
     };
---
+++ section-10-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ resTitle }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['resTitle'],
+};
+</script>
